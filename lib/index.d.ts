8e1992bffc01fd6697ae6e27f85010a8a881b94d
Ole Martin Pettersen <olemp@puzzlepart.com>
2017-09-28 11:30:26 +0200

    0.3.0

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -7,21 +7,17 @@ export interface IJsomContext {
     lists: SP.ListCollection;
     propBag: SP.FieldStringValues;
 }
-declare const JsomContext: {
-    Context: {
-        new (url: string): {
-            url: string;
-            isLoaded: boolean;
-            clientContext: SP.ClientContext;
-            web: SP.Web;
-            lists: SP.ListCollection;
-            propBag: SP.FieldStringValues;
-            load(): Promise<IJsomContext>;
-        };
-    };
-    ExecuteQuery: (ctx: IJsomContext, clientObjectsToLoad?: SP.ClientObject[]) => Promise<{
-        sender: any;
-        args: any;
-    }>;
-};
-export default JsomContext;
+export declare class JsomContext implements IJsomContext {
+    url: string;
+    isLoaded: boolean;
+    clientContext: SP.ClientContext;
+    web: SP.Web;
+    lists: SP.ListCollection;
+    propBag: SP.FieldStringValues;
+    constructor(url: string);
+    load(): Promise<IJsomContext>;
+}
+export declare function ExecuteQuery(ctx: IJsomContext, clientObjectsToLoad?: SP.ClientObject[]): Promise<{
+    sender: any;
+    args: any;
+}>;
--- src/index.ts
+++ src/index.ts
@@ -16,42 +16,40 @@ function __getClientContext(url: string) {
     });
 }
 
-const JsomContext = {
-    Context: class Context implements IJsomContext {
-        public url: string;
-        public isLoaded: boolean;
-        public clientContext: SP.ClientContext;
-        public web: SP.Web;
-        public lists: SP.ListCollection;
-        public propBag: SP.FieldStringValues;
+export class JsomContext implements IJsomContext {
+    public url: string;
+    public isLoaded: boolean;
+    public clientContext: SP.ClientContext;
+    public web: SP.Web;
+    public lists: SP.ListCollection;
+    public propBag: SP.FieldStringValues;
 
-        constructor(url: string) {
-            this.isLoaded = false;
-            this.url = url;
-        }
+    constructor(url: string) {
+        this.isLoaded = false;
+        this.url = url;
+    }
 
-        public async load(): Promise<IJsomContext> {
-            this.clientContext = await __getClientContext(this.url);
-            this.web = this.clientContext.get_web();
-            this.lists = this.web.get_lists();
-            this.propBag = this.web.get_allProperties();
-            return this;
-        }
-    },
-    ExecuteQuery: (ctx: IJsomContext, clientObjectsToLoad: SP.ClientObject[] = []) => {
-        return new Promise<{ sender, args }>((resolve, reject) => {
-            if (ctx.isLoaded) {
-                clientObjectsToLoad.forEach(clientObj => ctx.clientContext.load(clientObj));
-                ctx.clientContext.executeQueryAsync((sender, args) => {
-                    resolve({ sender, args });
-                }, (sender, args) => {
-                    reject({ sender, args });
-                })
-            } else {
-                reject();
-            }
-        });
+    public async load(): Promise<IJsomContext> {
+        this.clientContext = await __getClientContext(this.url);
+        this.web = this.clientContext.get_web();
+        this.lists = this.web.get_lists();
+        this.propBag = this.web.get_allProperties();
+        return this;
     }
 }
 
-export default JsomContext;
+export function ExecuteQuery(ctx: IJsomContext, clientObjectsToLoad: SP.ClientObject[] = []) {
+    return new Promise<{ sender, args }>((resolve, reject) => {
+        if (ctx.isLoaded) {
+            clientObjectsToLoad.forEach(clientObj => ctx.clientContext.load(clientObj));
+            ctx.clientContext.executeQueryAsync((sender, args) => {
+                resolve({ sender, args });
+            }, (sender, args) => {
+                reject({ sender, args });
+            })
+        } else {
+            reject();
+        }
+    });
+}
+
